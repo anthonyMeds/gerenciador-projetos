de83108017a43251b24ff79a8eb99a3f067a185a
anthonyMeds <anthony.eduardo@hotmail.com>
2025-01-27 21:24:34 -0300

    build: criação do projeto backend

---
+++ backend/src/test/java/com/gerenciador/projetos/GerenciadorDeProjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gerenciador.projetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorDeProjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
