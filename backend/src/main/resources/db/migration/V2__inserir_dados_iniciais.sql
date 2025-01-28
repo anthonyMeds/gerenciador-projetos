a107c42763a442447ce5724813ef55ec8c7f70cc
anthonyMeds <anthony.eduardo@hotmail.com>
2025-01-27 22:38:19 -0300

    config: inserção de dados iniciais de equipes e status descritos nos requisitos

---
+++ backend/src/main/resources/db/migration/V2__inserir_dados_iniciais.sql
@@ -0,0 +1,12 @@
+-- Inserir equipes responsáveis
+INSERT INTO equipes (nome) VALUES
+                               ('ADMFIN'),
+                               ('ADMPLN'),
+                               ('ADMAPO');
+
+-- Inserir status de projetos e tarefas
+INSERT INTO status (nome) VALUES
+                              ('Planejado'),
+                              ('Em execução'),
+                              ('Abortado'),
+                              ('Finalizado');
