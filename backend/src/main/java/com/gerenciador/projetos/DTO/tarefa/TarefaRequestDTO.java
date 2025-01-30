eb249260f50b5dac1c69fa88e8f84e3667239466
anthonyMeds <anthony.eduardo@hotmail.com>
2025-01-29 21:17:17 -0300

    feat: swagger request tarefas

--- backend/src/main/java/com/gerenciador/projetos/DTO/tarefa/TarefaRequestDTO.java
+++ backend/src/main/java/com/gerenciador/projetos/DTO/tarefa/TarefaRequestDTO.java
@@ -1,11 +1,35 @@
 package com.gerenciador.projetos.DTO.tarefa;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record TarefaRequestDTO(
+        @NotBlank(message = "O título é obrigatório")
+        @Size(max = 100, message = "Tamanho máximo do campo de 100 dígitos")
+        @Schema(example = "título teste")
         String titulo,
+
+        @NotBlank(message = "A descrição da tarefa é obrigatória")
+        @Size(max = 255, message = "Tamanho máximo do campo de 255 dígitos")
+        @Schema(example = "título teste")
         String descricao,
+
+        @NotNull(message = "O projeto é obrigatório")
+        @Schema(example = "1")
         Long projetoId,
+
+        @NotNull(message = "O responsável é obrigatório")
+        @Schema(example = "1")
         Long responsavelId,
+
+        @NotNull(message = "O prazo é obrigatório")
+        @Schema(example = "10")
         Integer prazoDias,
+
+        @NotNull(message = "O status é obrigatório")
+        @Schema(example = "1")
         Long statusId
 ) {
 }
