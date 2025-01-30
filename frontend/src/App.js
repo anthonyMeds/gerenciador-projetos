ff4343e94785734c399669ed18cb58d07b50072d
anthonyMeds <anthony.eduardo@hotmail.com>
2025-01-30 20:58:22 -0300

    feat: front end nav bar bootstrap

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,8 @@ import Home from "./components/Home";
 import Tarefas from "./components/Tarefas";
 import Projetos from "./components/Projetos";
 import { BrowserRouter, Routes, Link, Route } from "react-router-dom";
+import { Nav } from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
@@ -10,24 +12,29 @@ function App() {
       <p>teste</p>
 
       <BrowserRouter>
-        <ul>
-          <li>
-            <Link to="/"> Home </Link>
-          </li>
-          <li>
-            <Link to="/frontend/src/components/Tarefas"> Tarefas </Link>
-          </li>
-          <li>
-            <Link to="/frontend/src/components/Projetos"> Projetos </Link>
-          </li>
-        </ul>
+        <Nav variant="tabs">
+          <Nav.Link as={Link} to="/">
+            Pagina Inicial
+          </Nav.Link>
+          <Nav.Link as={Link} to="/frontend/src/components/Tarefas">
+            Gerenciamento de Tarefas
+          </Nav.Link>
+          <Nav.Link as={Link} to="/frontend/src/components/Projetos">
+            Gerenciamento de Projetos
+          </Nav.Link>
+        </Nav>
 
         <Routes>
-          <Route path="/" element={<Home/>}></Route>
-          <Route path="/frontend/src/components/Tarefas" element={<Tarefas/>}></Route>
-          <Route path="/frontend/src/components/Projetos" element={<Projetos/>}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route
+            path="/frontend/src/components/Tarefas"
+            element={<Tarefas />}
+          ></Route>
+          <Route
+            path="/frontend/src/components/Projetos"
+            element={<Projetos />}
+          ></Route>
         </Routes>
-
       </BrowserRouter>
     </div>
   );
