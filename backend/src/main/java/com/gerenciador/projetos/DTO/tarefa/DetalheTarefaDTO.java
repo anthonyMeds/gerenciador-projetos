95b1b06352035360e80f0c6f129317e1fd0df59e
anthonyMeds <anthony.eduardo@hotmail.com>
2025-01-29 21:11:15 -0300

    feat: tarefa dto

--- backend/src/main/java/com/gerenciador/projetos/DTO/tarefa/DetalheTarefaDTO.java
+++ backend/src/main/java/com/gerenciador/projetos/DTO/tarefa/DetalheTarefaDTO.java
@@ -1,2 +1,12 @@
-package com.gerenciador.projetos.DTO.tarefa;public record DetalheTarefaDTO() {
+package com.gerenciador.projetos.DTO.tarefa;
+
+public record DetalheTarefaDTO(
+        Long id,
+        String titulo,
+        String descricao,
+        String nomeProjeto,
+        String nomeResponsavel,
+        Integer prazoDias,
+        String statusNome
+) {
 }
--- backend/src/main/java/com/gerenciador/projetos/DTO/tarefa/TarefaRequestDTO.java
+++ backend/src/main/java/com/gerenciador/projetos/DTO/tarefa/TarefaRequestDTO.java
@@ -1,2 +1,11 @@
-package com.gerenciador.projetos.DTO.tarefa;public record TarefaRequestDTO() {
+package com.gerenciador.projetos.DTO.tarefa;
+
+public record TarefaRequestDTO(
+        String titulo,
+        String descricao,
+        Long projetoId,
+        Long responsavelId,
+        Integer prazoDias,
+        Long statusId
+) {
 }
