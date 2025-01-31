048e17198667b4d914b1796879faade5e26c0c95
anthonyMeds <anthony.eduardo@hotmail.com>
2025-01-30 21:57:34 -0300

    refactor: frontend usestate para tarefas

--- frontend/src/components/Tarefas.js
+++ frontend/src/components/Tarefas.js
@@ -1,12 +1,22 @@
 import React from "react";
-import { Table } from 'react-bootstrap';
+import { Table } from "react-bootstrap";
 
 class Tarefas extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      tarefas: [],
+      tarefas: [
+        {
+          id: 1,
+          titulo: "teste ss",
+          descricao: "desc",
+          nomeProjeto: "nometeste",
+          nomeResponsavel: "resp",
+          prazoDias: "10",
+          statusNome: "Planejado",
+        },
+      ],
     };
   }
 
@@ -18,22 +28,24 @@ class Tarefas extends React.Component {
             <th>Título</th>
             <th>Descrição</th>
             <th>Nome do Projeto</th>
-            <th>Responsável</th>
+            <th>Responsável da Tarefa</th>
             <th>Prazo</th>
             <th>Status</th>
             <th>Opções</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>@mdo</td>
-            <td>Atualizar Excluir</td>
-          </tr>
+          {this.state.tarefas.map((tarefas) => (
+            <tr>
+              <td> {tarefas.titulo} </td>
+              <td> {tarefas.descricao} </td>
+              <td> {tarefas.nomeProjeto} </td>
+              <td> {tarefas.nomeResponsavel} </td>
+              <td> {tarefas.prazoDias} </td>
+              <td> {tarefas.statusNome} </td>
+              <td> Atualizar Excluir </td>
+            </tr>
+          ))}
         </tbody>
       </Table>
     );
